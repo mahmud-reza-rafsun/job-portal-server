61f06527b117b2b1da3e64a3378928e1c126e84b
mahmud-reza-rafsun <rafsun16.it@gmail.com>
2025-06-15 13:23:18 +0600

    create get operation for specifice job id for details

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uuac6m8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("connected to MongoDB!");
+
+        // jobs related apis
+        const jobsCollections = client.db('jobPortal').collection('jobs');
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollections.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollections.findOne(query);
+            res.send(result);
+        })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('job is falling in the sky');
+})
+app.listen(port, () => {
+    console.log(`server is running on por: ${port}`);
+})
